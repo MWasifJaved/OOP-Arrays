f8a0ea75d8fb97a7c278cba197f469fb9b843564
MWasifJaved <wasif2024ag9444ai@gmail.com>
2026-01-12 19:24:55 +0500

    Add files via upload

---
+++ Two Dimensional Arrays - 2D/2d arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2d_arrays
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = {
+                              {10,20,30,40},
+                              { 50,60,70,80},
+                              { 90,100,110,120}
+
+            
+            };
+            Console.WriteLine(numbers.GetLength(0));
+            Console.WriteLine(numbers.GetLength(1));
+            Console.WriteLine(numbers[2,1]);
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+               for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    Console.Write(numbers[i,j] + " ");
+                }
+                Console.WriteLine();
+
+
+            }
+
+
+
+
+        }
+
+
+
+    }
+    }
+
